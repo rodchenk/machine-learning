66b049ac57c0b50ee57801fa84b123f732f84ccd
rodchenk <rodchenk@th-brandenburg.de>
2020-01-28 18:58:54 +0100

    add two layers and increase units

--- src/dense_layers.py
+++ src/dense_layers.py
@@ -12,10 +12,12 @@ def loss_graph(history):
 	plt.plot(history)
 
 if __name__ == '__main__':
-	layer = tf.keras.layers.Dense(units=1, input_shape=[1])
-	model = tf.keras.Sequential([layer])
+	layer_0 = tf.keras.layers.Dense(units=4, input_shape=[1])
+	layer_1 = tf.keras.layers.Dense(units=4)
+	layer_2 = tf.keras.layers.Dense(units=1)
+	model = tf.keras.Sequential([layer_0, layer_1, layer_2])
 	model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
 	history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
-	print(model.predict([10.0]))
+	print(model.predict([15.0]))
 
 	#loss_graph(history.history['loss'])
