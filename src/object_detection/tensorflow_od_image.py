ea448d38d211a621045ab082a361e2626e711407
rodchenk <rodchenk@th-brandenburg.de>
2020-02-16 16:15:20 +0100

    overwrite tensorflow image detection

--- src/object_detection/tensorflow_od_image.py
+++ src/object_detection/tensorflow_od_image.py
@@ -63,7 +63,7 @@ def load_image_into_numpy_array(image):
 # image2.jpg
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
 PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(5, 6) ]
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(6, 7) ]
 
 # Size, in inches, of the output images.
 IMAGE_SIZE = (12, 8)
